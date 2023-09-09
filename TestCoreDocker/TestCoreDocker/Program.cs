a44aebde1a4cb0bbf08d19913cefc82e44af26e9
sharif0rifat <sharif.rifat1986@gmail.com>
2023-09-08 22:52:33 -0700

    Added swagger for production

--- TestCoreDocker/TestCoreDocker/Program.cs
+++ TestCoreDocker/TestCoreDocker/Program.cs
@@ -14,7 +14,7 @@ builder.Services.AddTransient<IWeatherLab>(x => new WeatherLab(weatherType,forca
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+if (app.Environment.IsDevelopment()|| app.Environment.IsProduction())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
