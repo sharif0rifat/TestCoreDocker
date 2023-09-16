9e0fea384a9956ccea9472085a666e46884a0632
sharif0rifat <sharif.rifat1986@gmail.com>
2023-09-16 06:49:04 -0700

    Extended optio and add suppression for some warning

--- TestCoreDocker/TestCoreDockerService/Models/Options/WeatherOptions.cs
+++ TestCoreDocker/TestCoreDockerService/Models/Options/WeatherOptions.cs
@@ -5,9 +5,21 @@ namespace TestCoreDockerService.Models.Options;
 public class WeatherOptions
 {
     public const string OptionsKey = "WeatherOptions";
-    
+
     [Required] [MinLength(1)] [MaxLength(260)]
     public string WeatherType { get; init; } = "";
     [Required] [MinLength(3)] [MaxLength(260)]
     public string ForecastArea { get; init; } = "";
+    [Required]
+    [MinLength(3)]
+    [MaxLength(100)]
+    public string ForecastType { get; set; } = "";
+    [Required]
+    [MinLength(3)]
+    [MaxLength(1000)]
+    public string ApiKey { get; set; } = "";
+    [Required]
+    [MinLength(3)]
+    [MaxLength(1000)]
+    public string ApiBaseUrl { get; set; } = "";
 }
